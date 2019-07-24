d68fc63c69a871e8375fd60db19b6a8059884917
Amine Ben Asker <ben.asker.amine@gmail.com>
2019-07-24 18:08:41 +0200

    release 0.4.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except IOError:
 setup(
     name="config42",
     packages=find_packages(),
-    version="0.4.0",
+    version="0.4.1",
     license="GPLv3+",
     description="Configuration manager for cloud native application, support configuration stored"
                 " in memory, in files, in databases",
